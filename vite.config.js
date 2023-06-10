bd540a1a4f876c7fd7037da4b9388d13c5a87f0e
chengzelee <chengzelee@163.com>
2023-06-10 16:20:04 +0800

    增加部署配置

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // 配置前端服务地址和端口
+  // 配置前端服务地址和端口 只有本地运行的时候才需要
   server: {
     host: '127.0.0.1',
     port: 8888,
@@ -20,41 +20,10 @@ export default defineConfig({
     // 设置反向代理，解决跨域问题
     proxy: {
       '/blog': {
-        target: 'https://192.168.23.130',
+        target: 'https://192.168.23.130:1919',
         changeOrigin: true,
         secure: false,
       },
-/*      '/blog/auth': {
-        // 后台地址
-        target: 'http://192.168.23.130:8002',
-        // 是否跨域
-        changeOrigin: true,
-        // 是否https接口
-        secure: false,
-        // 是否代理 websocket
-        //ws: true,
-      },
-      '/blog/blogmanager': {
-        target: 'http://127.0.0.1:8041',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/blog/search': {
-        target: 'http://127.0.0.1:8004',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/blog/oss': {
-        target: 'http://127.0.0.1:8005',
-        changeOrigin: true,
-        secure: false,
-      },*/
-      // '/api2': {
-      //   // 后台地址
-      //   target: 'http://127.0.0.1:8956/',
-      //   changeOrigin: true,
-      //   rewrite: path => path.replace(/^\/api2/, '')
-      // }
     }
   },
   build: {
