433ccc76be4de14c8005174517d849cc42838953
chengzelee <chengzelee@163.com>
2023-06-12 00:36:18 +0800

    使用 WebHashHistory ，避免刷新页面因url不正确而报错

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
+// 使用 WebHashHistory ，避免刷新页面因url不正确而报错
+// https://router.vuejs.org/zh/guide/essentials/history-mode.html
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
